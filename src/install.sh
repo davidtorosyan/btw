cf2144f6e7862ecc3111672c96f9ddb4d21e6809
David Torosyan <davidtorosyan.git@gmail.com>
2022-12-26 15:52:12 -0800

    Add comment about removing partitions with parted

--- src/install.sh
+++ src/install.sh
@@ -56,6 +56,7 @@ timedatectl set-ntp true
 swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
 swap_end=$(( $swap_size + 129 + 1 ))MiB
 
+# In case this fails, use parted to delete the existing partitions then restart.
 parted --script "${device}" -- mklabel gpt \
   mkpart ESP fat32 1Mib 129MiB \
   set 1 boot on \
